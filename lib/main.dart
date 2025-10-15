352b07b79b1f3f2dbb7c50651cc13e0ed3657eee
Ibrahim Mohammed <hemamohamed.hm35@gmail.com>
2025-10-16 00:15:40 +0300

    Adds Firebase initialization to app startup
    Integrates Firebase alongside Supabase for expanded backend support.
    Enables usage of Firebase services by initializing during app launch.
    
    Adds Firebase initialization to app startup
    
    Integrates Firebase alongside Supabase to enable usage of Firebase services and expand backend capabilities during application launch.

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,18 @@ import 'package:chatter/feature/splash/presentation/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: ".env");
-  await Supabase.initialize(
+  
+  await Supabase.initialize(    
     url: dotenv.env['appLinkUrl']!,
     anonKey: dotenv.env['supabaseAnonKey']!,
+  );
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(Chatter());
 }
