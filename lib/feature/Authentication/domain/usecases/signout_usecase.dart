26632305322592f6c580e1b4e831640ec73247f4
Ibrahim Mohammed <hemamohamed.hm35@gmail.com>
2025-10-22 23:36:16 +0300

    Adds use cases for sign out and OTP verification
    Introduces dedicated use case classes to encapsulate sign out and OTP verification logic, improving separation of concerns and preparing for scalable authentication features.

--- lib/feature/Authentication/domain/usecases/signout_usecase.dart
+++ lib/feature/Authentication/domain/usecases/signout_usecase.dart
@@ -1,1 +1,11 @@
+import 'package:chatter/feature/Authentication/domain/repository/domain_auth_repo.dart';
 
+class SignOutUseCase {
+  AuthRepo authRepo;
+
+  SignOutUseCase(this.authRepo);
+
+  Future<void> call() async {
+    await authRepo.signOut();
+  }
+}
--- lib/feature/Authentication/domain/usecases/verify_otp_usecase.dart
+++ lib/feature/Authentication/domain/usecases/verify_otp_usecase.dart
@@ -1,1 +1,11 @@
+import 'package:chatter/feature/Authentication/domain/repository/domain_auth_repo.dart';
 
+class VerifyOtpUseCase {
+  AuthRepo authRepo;
+
+  VerifyOtpUseCase(this.authRepo);
+
+  Future<void> call(String smsCode) async {
+    await authRepo.verifyOtp(smsCode);
+  }
+}
