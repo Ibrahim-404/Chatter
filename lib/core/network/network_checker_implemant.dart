5ef0cbc5d0b77e3f31f81884e8d199ef14d4ec28
Ibrahim Mohammed <hemamohamed.hm35@gmail.com>
2025-12-24 22:59:16 +0200

    Integrates network connectivity check abstraction
    Introduces an abstract layer for network connectivity status using a popular internet checking library.
    Facilitates asynchronous connectivity checks to support improved network-aware application logic.
    
    Adds async network connectivity abstraction
    
    Introduces an abstract layer for checking network connectivity
    using an established internet connection library. Enables
    asynchronous status checks to support more robust network-aware
    application flows.

--- lib/core/network/network_checker_implemant.dart
+++ lib/core/network/network_checker_implemant.dart
@@ -1,6 +1,8 @@
+import 'package:chatter/core/network/network_checker.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
 
-abstract class NetworkChecker {
+class NetworkCheckerImplemant implements NetworkChecker {
+  @override
   Future<bool> get isConnected async {
     return await InternetConnectionChecker.instance.hasConnection;
   }
