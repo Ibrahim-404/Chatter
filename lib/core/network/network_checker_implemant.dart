fd39f8df5febc02638ccc61249c746351ad0c99e
Ibrahim Mohammed <hemamohamed.hm35@gmail.com>
2025-12-24 23:47:50 +0200

    Refactors network checker to support dependency injection
    Replaces direct singleton usage with injected dependency for improved testability and flexibility. Registers the network checker with dependency injection to enable easier mocking and configuration in different environments.
    
    Refactors network checker for dependency injection
    
    Enables dependency injection for network checking to improve testability and flexibility. Registers the network checker with the service locator, allowing easier mocking and configuration in different environments.

--- lib/core/network/network_checker_implemant.dart
+++ lib/core/network/network_checker_implemant.dart
@@ -2,8 +2,12 @@ import 'package:chatter/core/network/network_checker.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
 
 class NetworkCheckerImplemant implements NetworkChecker {
+  final InternetConnectionChecker connectionChecker;
+  
+  NetworkCheckerImplemant({required this.connectionChecker});
+
   @override
   Future<bool> get isConnected async {
-    return await InternetConnectionChecker.instance.hasConnection;
+    return await connectionChecker.hasConnection;
   }
 }
--- lib/feature/injection_container.dart
+++ lib/feature/injection_container.dart
@@ -1,3 +1,4 @@
+import 'package:chatter/core/network/network_checker_implemant.dart';
 import 'package:chatter/feature/Authentication/data/repository/Auth_Repo_dataLayer.dart';
 import 'package:chatter/feature/Authentication/domain/repository/domain_auth_repo.dart';
 import 'package:chatter/feature/Authentication/presentation/manager/auht_bloc/bloc/send_varify_bloc.dart';
@@ -6,7 +7,9 @@ import 'package:get_it/get_it.dart';
 
 final sl = GetIt.instance;
 Future<void> init() async {
+  sl.registerLazySingleton(() => NetworkCheckerImplemant(connectionChecker: sl()));
   sl.registerLazySingleton<AuthRepo>(() => AuthRepoDataLayer());
   sl.registerFactory(() => SendVarifyBloc(authRepo: sl()));
+
   // sl.registerFactory(() => ValidationBloc());
 }
